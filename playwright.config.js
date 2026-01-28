846ac93c42825f131548d83623f865a0e9672156
Amod Indupa <amodindupa9@gmail.com>
2026-01-28 16:59:52 +0530

    a0

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     // Run browser in headed mode so you can see SwiftTranslator while tests run
-    headless: false,
+    headless: true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
